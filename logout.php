fc620863d710078432be3065b89d6d868e6b3a01
Markusice <szekeres.mark03@gmail.com>
2023-11-29 18:22:14 +0100

    write session deletion correctly

--- logout.php
+++ logout.php
@@ -2,11 +2,18 @@
 
 require_once './utils.php';
 
-session_start([
-    'cookie_lifetime' => 86400,
-]);
+session_start();
 
 if (isset($_POST['logout'])) {
     session_destroy();
+
+    $_SESSION = array();
+
+    if (ini_get('session.use_cookies')) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params['path'], $params['domain'],
+            $params['secure'], $params['httponly']);
+    }
     redirect('index.php');
 }
